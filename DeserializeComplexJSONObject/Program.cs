c32b24389e0c9fff82617c44319ef3b19ed96d16
filipibc <filipibc@gmail.com>
2022-03-07 23:25:19 -0300

    Inicio do projeto

---
+++ DeserializeComplexJSONObject/Program.cs
@@ -0,0 +1,20 @@
+﻿using DeserializeComplexJSONObject.POCO;
+using System.Text.Json;
+
+namespace DeserializeComplexJSONObject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var json = ReadJsonFile();
+            var systemTextJson = JsonSerializer.Deserialize<Company>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        private static string ReadJsonFile()
+        {
+            using StreamReader reader = new (@$"{AppContext.BaseDirectory}\ComplexObject.json");
+            return reader.ReadToEnd();
+        }
+    }
+}
