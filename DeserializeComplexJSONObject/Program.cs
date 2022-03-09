1fcbf34202afe9b41ab96accc00bd56971734528
filipibc <filipibc@gmail.com>
2022-03-09 17:17:01 -0300

    - Instalação do Newtonsoft.Json - Refatoração de código

--- DeserializeComplexJSONObject/Program.cs
+++ DeserializeComplexJSONObject/Program.cs
@@ -7,13 +7,50 @@ namespace DeserializeComplexJSONObject
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("-------------------- Deserializing complex JSON object");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Reading JSON file");
             var json = ReadJsonFile();
-            var systemTextJson = JsonSerializer.Deserialize<Company>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Deserialize using System.Text.Json");
+            var companyUsingSystemTextJson = DeserializeUsingGenericSystemTextJson(json);
+
+            Console.WriteLine();
+
+            Console.WriteLine("---------- Deserialize using Newtonsoft.Json");
+            var companyUsingNewtonSoftJson = DeserializeUsingNewtonSoftJson(json);
+
+            Console.WriteLine("End of execution");
+        }
+
+        private static Company? DeserializeUsingNewtonSoftJson(string json)
+        {
+            var company = Newtonsoft.Json.JsonConvert.DeserializeObject<Company>(json);
+
+            return company;
+        }
+
+        private static Company? DeserializeUsingGenericSystemTextJson(string json)
+        {
+            var company = JsonSerializer.Deserialize<Company>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            return company;
+        }
+
+        private static Company? DeserializeUsingSystemTextJson(string json)
+        {
+            var company = (Company?)JsonSerializer.Deserialize(json, typeof(Company), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+            return company;
         }
 
         private static string ReadJsonFile()
         {
-            using StreamReader reader = new (@$"{AppContext.BaseDirectory}\ComplexObject.json");
+            using StreamReader reader = new(@$"{AppContext.BaseDirectory}\ComplexObject.json");
             return reader.ReadToEnd();
         }
     }
